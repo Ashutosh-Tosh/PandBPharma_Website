db72199c0e488203f84777601e2d477d5db6a25f
Ashutosh Patel <ashutoshdp49@gmail.com>
2023-08-09 17:53:40 +0530

    added raw navbar

---
+++ pnbpharma-website/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './common/navbar';
+function App() {
+  return (
+    <div>
+     <Navbar/>
+  </div>
+  );
+}
+
+export default App;
---
+++ pnbpharma-website/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Include your JavaScript/React files
+    "./public/index.html", // Include your HTML file
+    // Add more paths if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
