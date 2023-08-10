3fd37199d6b1d440699630655d0450c5f386747c
Ashutosh Patel <ashutoshdp49@gmail.com>
2023-08-10 14:52:10 +0530

    Project init with React and tailwind

--- pnbpharma-website/src/App.js
+++ pnbpharma-website/src/App.js
@@ -3,6 +3,9 @@ import Navbar from './common/navbar';
 function App() {
   return (
     <div>
+      <header className='h-20 text-6xl italic'>
+        P&B PHARMA CO LTD
+      </header>
      <Navbar/>
   </div>
   );
